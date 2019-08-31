26200c99b03157514c7de44ac97081b290022ba5
Nazmul Idris <nazmul@fasterlap.com>
2019-08-31 12:41:02 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 Nazmul Idris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("org.jetbrains.intellij") version "0.4.10"
+  kotlin("jvm") version "1.3.50"
+}
+
+group = "com.developerlife.example"
+version = "1.0"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib-jdk8"))
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+  version = "2019.2.1"
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+  changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
+tasks.withType<KotlinCompile> {
+  kotlinOptions.jvmTarget = "1.8"
+}
