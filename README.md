9ab0cc87c3d3fd1b0bddb3dc64053e33700fee15
Nazmul Idris <nazmul@fasterlap.com>
2019-08-31 19:44:20 -0700

    Add plugin icon, and icon for HelloAction

---
+++ README.md
@@ -0,0 +1,18 @@
+# References
+
+JetBrains IDEA docs
+
+- [Writing your first plugin](https://tinyurl.com/y67ygovg)
+- [Organizing and using icons in plugins](https://tinyurl.com/y33rbxst)
+- [github issues thread on IDEA icons](https://tinyurl.com/yxe8yhxt)
+- [List of default icons in IDEA, AllIcons.java](https://tinyurl.com/y4nh4nwu)
+
+Tutorials on writing plugins
+
+- [Simple custom dictionary loader plugin](https://tinyurl.com/y2n8ymsh)
+- [Simple stackoverflow lookup plugin](https://tinyurl.com/y336wul6)
+
+Icons
+
+- [Material Design Icons](https://tinyurl.com/y4op6mnt)
+- [SVG resizing](https://tinyurl.com/y6mzgofw)
---
+++ src/main/kotlin/PluginIcons.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 Nazmul Idris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+/**
+Provides programmatic access to the SVG icons, in the `resources`
+folder, eg: `/icons/ic_check_circle.svg`
+ */
+interface PluginIcons {
+  val HELLO_ACTION: Icon
+    get() = IconLoader.getIcon("/icons/ic_check_circle.svg")
+}
