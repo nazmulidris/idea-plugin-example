3db9936c04e792f6d194816d93b7d8264a3d1290
Nazmul Idris <nazmul@google.com>
2020-04-10 15:47:23 -0700

    Fix version info in plugin
    Fix the problem preventing the plugin from being installed in IDEA
    by updating both the plugin.xml and build.gradle.kts to match.
    
    For build.gradle.kts:
    - `2020.1` is used for IDEA
    - `201.6668.27` is used for the Markdown plugin / module dependency
    
    For plugin.xml,
    - since-build = `2020.1`
    - until-build = `2020.*`

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,13 +39,13 @@ intellij {
   // You can use release build numbers or snapshot name for the version.
   // 1) IJ Release Repository w/ build numbers https://www.jetbrains.com/intellij-repository/releases/
   // 2) IJ Snapshots Repository w/ snapshot names https://www.jetbrains.com/intellij-repository/snapshots/
-  version = "193.6494.35" // You can also use LATEST-EAP-SNAPSHOT here.
+  version = "2020.1" // You can also use LATEST-EAP-SNAPSHOT here.
 
   // Declare a dependency on the markdown plugin to be able to access the
   // MarkdownRecursiveElementVisitor.kt file. More info:
   // https://www.jetbrains.org/intellij/sdk/docs/basics/plugin_structure/plugin_dependencies.html
   // https://plugins.jetbrains.com/plugin/7793-markdown/versions
-  setPlugins("java", "org.intellij.plugins.markdown:193.5233.63")
+  setPlugins("java", "org.intellij.plugins.markdown:201.6668.27")
 }
 
 tasks.withType<KotlinCompile> {
